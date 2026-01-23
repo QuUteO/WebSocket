7769d1d5709e08361e1978c2e29a92a6303cd1e3
QuUteO <quuteo86@gmail.com>
2026-01-23 14:49:04 +0300

    model

---
+++ internal/model/dto.go
@@ -0,0 +1,7 @@
+package model
+
+type WsMessage struct {
+	IPAddress string `json:"address"`
+	Message   string `json:"message"`
+	Time      string `json:"time"`
+}
---
+++ internal/model/model.go
@@ -0,0 +1,30 @@
+package model
+
+import "github.com/gofrs/uuid"
+
+// User Структура для работы с БД
+type User struct {
+	Id       uuid.UUID `db:"id"`
+	Email    string    `db:"email"`
+	Password string    `db:"password"`
+}
+
+// DTOResponse Структура server для ответа
+type DTOResponse struct {
+	ID    uuid.UUID `json:"id"`
+	Email string    `json:"email"`
+}
+
+// DTORequest Структура server для запроса
+type DTORequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Response Структура для ответа с сервера
+type Response struct {
+	StatusCode int         `json:"statusCode"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	Error      string      `json:"error"`
+}
