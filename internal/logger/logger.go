9b426558032f1956efae0d47eb276cd5ce0104be
QuUteO <quuteo86@gmail.com>
2026-01-23 14:50:03 +0300

    something

---
+++ internal/logger/logger.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func New(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case "local":
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "development":
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelWarn}))
+	}
+	return logger
+}
---
+++ pkg/db/postgres.go
@@ -0,0 +1,32 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/QuUteO/video-communication/internal/config"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+}
+
+func NewClient(ctx context.Context, ps *config.Postgres) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%v/%s", ps.User, ps.Password, ps.Host, ps.Port, ps.DB)
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	pool, err = pgxpool.Connect(ctx, dsn)
+	cancel()
+	if err == nil {
+		return pool, nil
+	}
+
+	return nil, fmt.Errorf("failed to connect to database: %w", err)
+}
